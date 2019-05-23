bb58f0ee20804daf56354f1460dedd7c02ed258d
HYE_JI <lwiiiiii@naver.com>
2019-05-23 16:07:17 +0900

    Student login 기능, test code 구현

--- src/main/java/com/shouwn/oj/controller/member/StudentController.java
+++ src/main/java/com/shouwn/oj/controller/member/StudentController.java
@@ -1,7 +1,109 @@
 package com.shouwn.oj.controller.member;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.shouwn.oj.exception.member.*;
+import com.shouwn.oj.model.entity.member.Student;
+import com.shouwn.oj.model.request.StudentSignUpRequest;
+import com.shouwn.oj.model.request.member.MemberLoginRequest;
+import com.shouwn.oj.model.response.ApiDataBuilder;
+import com.shouwn.oj.model.response.ApiResponse;
+import com.shouwn.oj.model.response.CommonResponse;
+import com.shouwn.oj.model.response.StudentInformation;
+import com.shouwn.oj.security.JwtProvider;
+import com.shouwn.oj.service.member.StudentServiceForApi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api")
 public class StudentController {
+
+	private final JwtProvider jwtProvider;
+
+	private  final StudentServiceForApi studentServiceForApi;
+
+	public StudentController(JwtProvider jwtProvider, StudentServiceForApi studentServiceForApi) {
+		this.jwtProvider = jwtProvider;
+		this.studentServiceForApi = studentServiceForApi;
+	}
+
+	@PostMapping
+	public ApiResponse<?> makeStudent(@RequestBody StudentSignUpRequest signUpRequest) {
+		try {
+			Student student = studentServiceForApi.makeStudent(
+					signUpRequest.getName(),
+					signUpRequest.getUsername(),
+					signUpRequest.getPassword(),
+					signUpRequest.getEmail()
+			);
+
+			return CommonResponse.builder()
+					.status(HttpStatus.CREATED)
+					.message("학생 생성 성공")
+					.build();
+
+		} catch (UsernameExistException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.CONFLICT)
+					.message("아이디 중복")
+					.build();
+		} catch (PasswordStrengthLeakException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.PRECONDITION_FAILED)
+					.message("비밀번호 강도가 약함")
+					.build();
+		} catch (EmailExistException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.CONFLICT)
+					.message("이메일 중복")
+					.build();
+		}
+	}
+
+	@PostMapping("login")
+	public ApiResponse<?> login(@RequestBody MemberLoginRequest loginRequest) {
+		Student student;
+
+		try {
+			student = studentServiceForApi.login(loginRequest.getUsername(), loginRequest.getPassword());
+		} catch (UsernameNotExistException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.PRECONDITION_FAILED)
+					.message(loginRequest.getUsername() + " 에 해당하는 사용자 아이디가 존재하지 않습니다.")
+					.build();
+		} catch (PasswordIncorrectException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.FORBIDDEN)
+					.message("비밀번호가 일치하지 않습니다.")
+					.build();
+		}
+
+		String jwt = jwtProvider.generateJwt(student.getId());
+
+		return CommonResponse.builder()
+				.status(HttpStatus.CREATED)
+				.message("로그인 성공")
+				.data(new ApiDataBuilder().addData("token", jwt).packaging())
+				.build();
+	}
+
+	@GetMapping("self")
+	@PreAuthorize("isAuthenticated()")
+	public ApiResponse<?> getSelfInformation(@RequestAttribute Long requestId) {
+		Student student = studentServiceForApi.findById(requestId);
+
+		StudentInformation studentInformation = StudentInformation.builder()
+				.name(student.getName())
+				.username(student.getUsername())
+				.email(student.getEmail())
+				.build();
+
+		return CommonResponse.builder()
+				.status(HttpStatus.OK)
+				.message("개인정보 조회 성공")
+				.data(studentInformation)
+				.build();
+	}
+
 }
--- src/main/java/com/shouwn/oj/service/member/StudentServiceForApi.java
+++ src/main/java/com/shouwn/oj/service/member/StudentServiceForApi.java
@@ -1,7 +1,54 @@
 package com.shouwn.oj.service.member;
 
+import com.shouwn.oj.exception.member.MemberException;
+import com.shouwn.oj.exception.member.PasswordIncorrectException;
+import com.shouwn.oj.model.entity.member.Student;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceForApi {
+
+	private final StudentService studentService;
+
+	public StudentServiceForApi(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	/**
+	 * 학생을 생성하는 메소드
+	 *
+	 * @param name		  학생 이름
+	 * @param username	  학생 아이디
+	 * @param rawPassword 학생 패스워드 (인코딩 전)
+	 * @param email		  학생 이메일
+	 *
+	 * @return 생성된 관리자 객체
+	 *
+	 * @throws MemberException UsernameExistException 이미 아이디가 존재할 때 발생하는 예외
+	 * 	                       PasswordStrengthLeakException 비밀번호가 약할 때 발생하는 예외
+	 * 	                       EmailExistException 이메일이 이미 존재할 때 발생하는 예외
+	 */
+
+	public Student makeStudent(String name,
+							   String username,
+							   String rawPassword,
+							   String email) throws MemberException {
+
+		return studentService.makeStudent(name, username, rawPassword, email);
+	}
+
+	public Student login(String username, String rawPassword) throws MemberException {
+		Student student = studentService.findByUsername(username);
+
+		if(!studentService.isCorrectPassword(student, rawPassword)) {
+			throw new PasswordIncorrectException("패스워드가 맞지 않습니다.");
+		}
+
+		return student;
+	}
+
+	public Student findById(Long id) {
+		return studentService.findById(id);
+	}
 }
