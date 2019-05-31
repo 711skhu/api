f745ddef5546b94688f5787815ab6363ee20b181
HYE_JI <lwiiiiii@naver.com>
2019-06-01 01:34:22 +0900

    #4 CourseListResponse Builder 어노테이션 제거

--- src/main/java/com/shouwn/oj/model/response/CourseListResponse.java
+++ src/main/java/com/shouwn/oj/model/response/CourseListResponse.java
@@ -1,7 +1,6 @@
 package com.shouwn.oj.model.response;
 
 import com.shouwn.oj.model.entity.problem.Course;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -13,7 +12,6 @@ public class CourseListResponse {
 
 	private String professorName;
 
-	@Builder
 	public CourseListResponse(Course course) {
 		this.id = course.getId();
 		this.name = course.getName();
